ad3a37451d22123816b5133e11fd79a13aa7d403
Harpreet Singh <hpt09@live.com>
2024-12-31 17:41:43 +1300

    Run on all pages

--- OpenYoutubeEmbed.js
+++ OpenYoutubeEmbed.js
@@ -4,7 +4,7 @@
 // @version      2024-10-02
 // @description  try to take over the world!
 // @author       You
-// @match        *://*.youtube.com/*
+// @match        *://*/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=youtube.com
 // @grant        none
 // ==/UserScript==
